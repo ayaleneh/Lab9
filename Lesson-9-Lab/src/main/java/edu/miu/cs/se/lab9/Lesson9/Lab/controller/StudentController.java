3d846c2978b74a37889f9c1f6f0240180d6e2827
Ayalneh <getaunayaleneh@gmail.com>
2023-06-16 14:05:56 -0500

    updated Lab 11

--- Lesson-9-Lab/src/main/java/edu/miu/cs/se/lab9/Lesson9/Lab/controller/StudentController.java
+++ Lesson-9-Lab/src/main/java/edu/miu/cs/se/lab9/Lesson9/Lab/controller/StudentController.java
@@ -2,6 +2,8 @@ package edu.miu.cs.se.lab9.Lesson9.Lab.controller;
 
 import edu.miu.cs.se.lab9.Lesson9.Lab.domain.Student;
 import edu.miu.cs.se.lab9.Lesson9.Lab.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,29 +16,48 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
     @GetMapping("/")
-    public List<Student> getAllStudents(){
-        return studentService.getAllStudents();
+    public ResponseEntity<?> getAllStudents() {
+        try {
+            return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
     @GetMapping("/{studentId}")
-    public Student getStudentById(@PathVariable Long studentId){
-       return studentService.getStudentById(studentId);
+    public ResponseEntity<?> getStudentById(@PathVariable Long studentId) {
+        try {
+            return new ResponseEntity<>(studentService.getStudentById(studentId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
     @PostMapping("/")
-    public Student addNewStudent(@RequestBody Student student){
-        return studentService.addNewStudent(student);
+    public ResponseEntity<Student> addNewStudent(@RequestBody Student student) {
+        return ResponseEntity.ok(studentService.addNewStudent(student));
     }
+
     @PutMapping("/{studentId}")
-    public Student updateExisingStudent(@PathVariable Long studentId, @RequestBody Student student){
-        return studentService.updateExistingStudentById(studentId,student);
+    public ResponseEntity<Student> updateExisingStudent(@PathVariable Long studentId, @RequestBody Student student) {
+        return ResponseEntity.ok(studentService.updateExistingStudentById(studentId, student));
     }
+
     @DeleteMapping("/{studentId}")
-    public void deleteStudentById(@PathVariable Long studentId){
-        studentService.deleteStudentById(studentId);
+    public ResponseEntity<?> deleteStudentById(@PathVariable Long studentId) {
+        try {
+            studentService.deleteStudentById(studentId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
     @GetMapping("/search")
-    public List<Student> searchStudentByFirstName(@RequestParam String firstname){
-        return studentService.searchStudentsByFirstName(firstname);
+    public ResponseEntity<List<Student>> searchStudentByFirstName(@RequestParam String firstname) {
+        return new ResponseEntity<>(studentService.searchStudentsByFirstName(firstname),HttpStatus.OK);
     }
 
 }
